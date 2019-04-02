6aa1f0cf05cd2c1cec0d9f5e89543faf5e7e538d
Alex Moon <alex.jared.moon@gmail.com>
2019-04-02 08:15:00 +0000

    feat: add contact form component

---
+++ src/components/formContact/formContact.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import styles from './formContact.module.scss';
+
+const FormContact = () => (
+  <div className={styles.container}>
+    <form
+      name="contact"
+      method="POST"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+      className={styles.contact}
+    >
+      <p hidden>
+        <label htmlFor="bot-field">
+          <span>
+            Don&#8217;t fill this out <br />
+            if you&#8217;re human:
+          </span>
+          <input name="bot-field" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="name">
+          <span>Your Name:</span>
+
+          <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="email">
+          <span>Your Email:</span>
+          <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="message">
+          <span> Message:</span> <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button id={styles.submit} type="submit">
+          Send
+        </button>
+      </p>
+    </form>
+  </div>
+);
+
+FormContact.propTypes = {};
+
+export default FormContact;
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Icon from 'components/icon';
+import ExtLink from 'components/extLink';
+import FormContact from 'components/formContact';
+
 import icons from 'constants/icons';
 
 import 'styles/style.scss';
-
 import styles from './layout.module.scss';
 
 const Layout = ({ children }) => (
@@ -13,8 +16,17 @@ const Layout = ({ children }) => (
       <div className={styles.children}>{children}</div>
     </main>
     <footer className={styles.footer}>
-      <Icon icon={icons.TWITTER} />
-      <Icon icon={icons.GITHUB} />
+      <div id="form-container" className={styles.half}>
+        <FormContact />
+      </div>
+      <div id={styles.social} className={styles.half}>
+        <ExtLink href="https://github.com/moonmeister">
+          <Icon icon={icons.GITHUB} />
+        </ExtLink>
+        <ExtLink href="https://twitter.com/moon_meister">
+          <Icon icon={icons.TWITTER} />
+        </ExtLink>
+      </div>
     </footer>
   </>
 );

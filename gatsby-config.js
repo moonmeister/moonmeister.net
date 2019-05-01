aaed18abdc079b354c60c2d413834ad17f3a7fb8
Alex Moon <alex.jared.moon@gmail.com>
2019-05-01 09:50:33 +0000

    fix: fix pwa colors enable offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,8 +63,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#efefef`,
-        theme_color: `#663399`,
+        background_color: `#f0f0f0`,
+        theme_color: `#665577`,
         display: `minimal-ui`,
         icon: `src/images/terminal-solid.svg`, // This path is relative to the root of the site.
       },
@@ -96,6 +96,6 @@ module.exports = {
     `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 };

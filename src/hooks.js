05c223b32a6cd2938645b20d987c7ac5aede09ec
Alex Moon <alex.jared.moon@gmail.com>
2022-08-23 22:11:26 +0000

    fixes

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,13 @@
 import * as Sentry from '@sentry/browser';
 
+Sentry.init({
+    dsn: 'https://5a510f4ea2b74d43b1ee4698b86ccbe8@o1374822.ingest.sentry.io/6682406',
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
 /** @type {import('@sveltejs/kit').HandleError} */
 export function handleError({ error, event }) {
     // example integration with https://sentry.io/
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -11,7 +11,7 @@
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 10.0,
+    tracesSampleRate: 1.0,
   });
 
   Sentry.captureException($page.error);

d1d2fb6476d1c0bda477b961213badda56dd903c
Alex Moon <moonmeister@users.noreply.github.com>
2024-04-01 16:44:29 +0000

    refaactor: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import netlify from '@astrojs/netlify/functions';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-	site: 'https://www.moonmeister.net',
-	integrations: [tailwind()],
-	adapter: netlify({
-		builders: true,
-	}),
+  output: 'hybrid',
+  site: 'https://www.moonmeister.net',
+  integrations: [tailwind(), sitemap({
+		changefreq: 'weekly',
+		priority: 0.7,
+		lastmod: new Date(),
+	})],
+  adapter: netlify({
+    builders: true
+  })
 });

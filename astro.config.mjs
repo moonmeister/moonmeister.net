9410732ff42e79b6f5f9fed444a991f4d3a85c99
Alex Moon <alex.jared.moon@gmail.com>
2025-11-11 20:58:35 -0800

    fix(config): change output type to 'static' and update netlify adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  site: 'https://www.moonmeister.net',
-  integrations: [tailwind(), sitemap({
-		changefreq: 'weekly',
-		priority: 0.7,
-		lastmod: new Date(),
-	})],
-  adapter: netlify({
-    builders: true
-  })
+	output: 'static',
+	site: 'https://www.moonmeister.net',
+	integrations: [
+		tailwind(),
+		sitemap({
+			changefreq: 'weekly',
+			priority: 0.7,
+			lastmod: new Date(),
+		}),
+	],
+	adapter: netlify(),
 });
